9a56411f1fbb930f8e9a25a695942abfc57eef18
Scott Crisenbery <scott@scrisenbery.net>
2016-07-29 16:04:52 -0700

    Added Maloy's bash configs [TODO review it all]

---
+++ bash/bashrc.d/50-pyenv.bash
@@ -0,0 +1,3 @@
+pathprepend "$HOME/.pyenv/bin"
+export PYENV_ROOT="$HOME/.pyenv"
+eval "$(pyenv init -)"
---
+++ bash/bashrc.d/80-ls-aliases.bash
@@ -0,0 +1,4 @@
+# The classic ls aliases
+alias ll='ls -alF --color=always'
+alias la='ls -A'
+alias l='ls -CF'
