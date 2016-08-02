b0e9c92ad4d2d19b28656175e412fc0db25872d2
Scott Crisenbery <scott@scrisenbery.net>
2016-08-01 17:51:53 -0700

    TODOs

--- install.d/30-yos-shiftit.sh
+++ install.d/30-yos-shiftit.sh
@@ -2,6 +2,8 @@
 # Ignore if we're not on OSX
 [[ "$OSTYPE" =~ ^darwin ]] || exit 0
 # //TODO
+
+# //TODO Explore if I can update the hotkeys during setup
 open ~/Applications/ShiftIt.app
 # This enables shiftit in security preferences, no seriously it does.
 sudo sqlite3 "/Library/Application Support/com.apple.TCC/TCC.db" 'update access set allowed="1" where client like "%shiftit%";'
