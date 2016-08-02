a131e15e7e2b69ee26bb7569bea7e71f21405c34
Scott Crisenbery <scott@scrisenbery.net>
2016-08-01 17:27:54 -0700

    Removed Sublime Text config

--- install.d/30-yos-sublime-text-3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Ignore if we're not on OSX
-[[ "$OSTYPE" =~ ^darwin ]] || exit 0
-
-# Configure our sublime to use the wbond package manager
-curl -o ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package https://sublime.wbond.net/Package%20Control.sublime-package $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages
