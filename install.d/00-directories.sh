3bf0486b4d435b049873d7370b01864a5bcb83e3
Scott Crisenbery <scott@scrisenbery.net>
2016-08-01 17:07:53 -0700

    Removed unwanted items

--- install.d/00-directories.sh
+++ install.d/00-directories.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-mkdir -p $HOME/Downloads/Torrents/Incomplete
-mkdir -p $HOME/Downloads/Torrents/Complete
-mkdir -p $HOME/Downloads/Chrome
-mkdir -p $HOME/Downloads/Firefox
-mkdir -p $HOME/Documents/Personal/Development
-mkdir -p $HOME/Documents/Professional
-mkdir -p $HOME/Dropbox
 mkdir -p $HOME/Google\ Drive
 mkdir -p $HOME/.shellrc
 cp -rn $HOME/.dotfiles/homeConfigFiles/.config $HOME/.config
