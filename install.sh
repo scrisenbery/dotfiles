8bcc91639a1129b0a0899f490c946550ab83dc09
Scott Crisenbery <scott@scrisenbery.net>
2016-08-08 09:59:17 -0700

    Updated some dir pathing

--- install.sh
+++ install.sh
@@ -6,11 +6,13 @@ copy_file() {
   source="${PWD}/$1"
   target="${HOME}/${1/_/.}"
 
+  mkdir ${HOME}/.dotbak/
+
   if [ -e $target ] ; then
     if [ ! -d $target ] ; then
       echo "Update\t$target"
       # //TODO This is backing up the original file if it exists. Need to store in a more specific location I think
-      mv $target $target.bak
+      mv $target .dotbak/$target.bak
       cp ${source} ${target}
     fi
   else
@@ -29,7 +31,7 @@ done
 set -ev
 
 # here we go and iterate over all the shell scripts in the install.d file and run them
-for file in $HOME/.dotfiles/install.d/*.sh; do
+for file in ${PWD}/setup.d/*.sh; do
   bash $file
 done
 
