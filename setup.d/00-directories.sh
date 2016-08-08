abb42bc29a939180ac07141f005f5b58b2c9c658
Scott Crisenbery <scott@scrisenbery.net>
2016-08-08 11:19:41 -0700

    Remove lines superceded by install script

--- setup.d/00-directories.sh
+++ setup.d/00-directories.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 mkdir -p $HOME/Google\ Drive
-mkdir -p $HOME/.shellrc
-cp -rn $HOME/.dotfiles/homeConfigFiles/.config $HOME/.config
-cp  $HOME/.dotfiles/homeConfigFiles/bashrc $HOME/.bashrc
-cp  $HOME/.dotfiles/homeConfigFiles/bash_profile $HOME/.bash_profile
-cp -rn $HOME/.dotfiles/homeConfigFiles/.ssh $HOME/.ssh
-cp  $HOME/.dotfiles/homeConfigFiles/.gitconfig $HOME/.gitconfig
