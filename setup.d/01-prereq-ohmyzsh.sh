c71f50f0f595e218a210f96d41ffa5a4b7c2a36e
Scott Crisenbery <scott@scrisenbery.net>
2016-08-09 10:37:11 -0700

    Updated, added TODO

--- setup.d/01-prereq-ohmyzsh.sh
+++ setup.d/01-prereq-ohmyzsh.sh
@@ -1,5 +1,4 @@
+#! /bin/sh
 # Install Oh-My-ZSH
-if [ ! -d $HOME/.vim/bundle/neobundle.vim ]
-then
-  bash -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
+# TODO check for existing install? Or will it do that itself?
+curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
