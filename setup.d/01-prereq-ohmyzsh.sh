32a91e6f6f587222e651bb21a15cb6fa2366d23d
Scott Crisenbery <scott@scrisenbery.net>
2016-08-09 11:35:13 -0700

    Updated path

--- setup.d/01-prereq-ohmyzsh.sh
+++ setup.d/01-prereq-ohmyzsh.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 # Install Oh-My-ZSH
 # TODO check for existing install? Or will it do that itself?
+export ZSH="$HOME/.shellrc.d/oh-my-zsh"
 curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
