93fcae057ec566a257a0cce7c29f18bbbaa57fd8
Scott Crisenbery <scott@scrisenbery.net>
2016-08-09 10:51:17 -0700

    Updated, resolved TODO

--- setup.d/01-prereq-powerlinefonts.sh
+++ setup.d/01-prereq-powerlinefonts.sh
@@ -1,1 +1,10 @@
-# //TODO
+# /bin/sh
+# Install Powerline Fonts
+
+echo "--------"
+echo "Installing Powerline Fonts"
+echo "--------"
+
+git clone https://github.com/powerline/fonts.git /tmp/powerline-fonts
+
+source /tmp/powerline-fonts/install.sh
