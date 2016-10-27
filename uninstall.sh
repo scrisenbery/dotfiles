5e8fd039a6a60d979b56113565541192f31bc393
Scott Crisenbery <scott@scrisenbery.net>
2016-10-26 18:32:20 -0700

    Remove TODO about false syntax error

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# //TODO Investigate why the /_/.  is being highlighted as if it's a syntax error (yet appears to work fine); this also applies to install script (but no TODO there because lazy)
 # It's doing a sed replacement. Given file path 'i' replace any _ with .
 
 for i in ${PWD}/config.d/_*
