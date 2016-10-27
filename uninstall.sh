62b917d39c12eeeabcd5492615ad7fd7c67b2bce
Scott Crisenbery <scott@scrisenbery.net>
2016-10-26 18:27:09 -0700

    Updated for directory structure

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # //TODO Investigate why the /_/.  is being highlighted as if it's a syntax error (yet appears to work fine); this also applies to install script (but no TODO there because lazy)
-for i in _*
+# It's doing a sed replacement. Given file path 'i' replace any _ with .
+
+for i in ${PWD}/config.d/_*
 do
-  rm -fr "${HOME}/${i/_/.}"
+  rm -rf "${HOME}/${i/_/.}"
 done
+
+# //TODO Restore backups? Note, this depends on the backup process preserving directory structure
