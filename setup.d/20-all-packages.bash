4fef52cc089eca80291e845a621317df7ece8778
Scott Crisenbery <scott@scrisenbery.net>
2016-10-27 17:31:25 -0700

    Resolved TODO, commented out all packages

--- setup.d/20-all-packages.bash
+++ setup.d/20-all-packages.bash
@@ -1,11 +1,9 @@
 #!/bin/ bash
 
-# //TODO Review, wtf is this
+#git clone https://github.com/yyuu/pyenv.git            ~/.pyenv
 
-git clone https://github.com/yyuu/pyenv.git            ~/.pyenv
+#git clone https://github.com/tokuhirom/plenv.git       ~/.plenv
+#git clone https://github.com/tokuhirom/Perl-Build.git  ~/.plenv/plugins/perl-build/
 
-git clone https://github.com/tokuhirom/plenv.git       ~/.plenv
-git clone https://github.com/tokuhirom/Perl-Build.git  ~/.plenv/plugins/perl-build/
-
-git clone https://github.com/rbenv/rbenv.git           ~/.rbenv
-git clone https://github.com/rbenv/ruby-build.git      ~/.rbenv/plugins/ruby-build
+#git clone https://github.com/rbenv/rbenv.git           ~/.rbenv
+#git clone https://github.com/rbenv/ruby-build.git      ~/.rbenv/plugins/ruby-build
