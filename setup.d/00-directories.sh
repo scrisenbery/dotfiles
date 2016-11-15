572d4dd162f7e6e3f0c2d53b76f6342ae5eab044
Scott Crisenbery <scott@scrisenbery.net>
2016-11-14 17:30:29 -0800

    Removed brew cask dir, should be auto-created

--- setup.d/00-directories.sh
+++ setup.d/00-directories.sh
@@ -5,5 +5,5 @@
 mkdir -pv $HOME/Google\ Drive
 mkdir -pv $HOME/projects
 
-# //TODO Test if the dir needs to exist beforehand
-mkdir -pv $HOME/homebrew-cask/Caskroom
+# This should be auto-created
+#mkdir -pv $HOME/homebrew-cask/Caskroom
