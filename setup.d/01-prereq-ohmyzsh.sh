caefbf59a4ffc4e2cce6eef641f36cae358bf901
Scott Crisenbery <scott@scrisenbery.net>
2017-05-02 12:41:08 -0700

    Made the script actually run

--- setup.d/01-prereq-ohmyzsh.sh
+++ setup.d/01-prereq-ohmyzsh.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
 # Install Oh-My-ZSH
-
-export ZSH="$HOME/.shellrc.d/oh-my-zsh"
-curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+echo "Install oh-my-zsh/n/n"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
