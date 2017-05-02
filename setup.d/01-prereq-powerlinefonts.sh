12c52fdf4f11e087d38c1fabffe62923665b33ae
Scott Crisenbery <scott@scrisenbery.net>
2017-05-02 12:54:01 -0700

    Updated to clean up after itself. Tested, working.

--- setup.d/01-prereq-powerlinefonts.sh
+++ setup.d/01-prereq-powerlinefonts.sh
@@ -6,5 +6,9 @@ echo "Installing Powerline Fonts"
 echo "--------"
 
 git clone https://github.com/powerline/fonts.git /tmp/powerline-fonts
-
 source /tmp/powerline-fonts/install.sh
+
+echo "Cleaning up temp dir /tmp/powerline-fonts"
+rm -rf /tmp/powerline-fonts
+
+echo "Installed Powerline Fonts"
