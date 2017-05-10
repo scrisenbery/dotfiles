6f7ceffbbf546107d4f5b88d9d5289ca4bf39530
Scott Crisenbery <scott@scrisenbery.net>
2017-05-10 14:56:54 -0700

    Fix issues with install for #2 - Change from bash to sh - Fix some flags - Comment uneeded stuff - Add TODOs

--- setup.d/20-mac-packages.sh
+++ setup.d/20-mac-packages.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 # Ignore if we're not on OSX
 [[ "$OSTYPE" =~ ^darwin ]] || exit 0
 
 # Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
- while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # //TODO why
 # Ask for the administrator password upfront.
@@ -15,18 +15,18 @@ brew update
 
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
+brew install coreutils # //TODO
 
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils --with-default-names
+brew install findutils --with-default-names # //TODO
 
-# Install more recent versions of some OS X tools and #JustSysAdminThings
+# Install more recent versions of some OS X tools and #JustSysAdminThings //TODO
 brew install gnu-indent --with-default-names
 brew install gnu-sed --with-default-names
-brew install gnutls --with-default-names
+brew install gnutls
 brew install gnu-tar --with-default-names
 brew install gnu-which --with-default-names
-brew install grep --with-defualt-names
+brew install grep --with-default-names
 brew install gawk
 brew install tree
 brew install htop
@@ -36,19 +36,17 @@ brew install terminal-notifier
 brew install lastpass-cli
 
 # This will install all the stuff we need for reasons
-brew cask install google-chrome
-brew cask install dropbox
+#brew cask install google-chrome
 brew cask install shiftit
 brew cask install alfred
-brew cask install google-drive
+#brew cask install google-drive
 brew cask install skype
 brew cask install slack
-brew cask install wireshark
-brew cask install vlc
+#brew cask install wireshark
+#brew cask install vlc
 
 # Work stuff
-brew install net-snmp
-
+#brew install net-snmp
 
 # Remove outdated versions from the cellar.
 brew cleanup
