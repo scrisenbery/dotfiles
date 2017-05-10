c151defee4dc77c1d047087bbf4dd22868fe847b
Scott Crisenbery <scott@scrisenbery.net>
2017-05-10 12:38:58 -0700

    User config changes from testing for #2 - Change from bash to sh - Comment everything since I don't have a current way to do the picture work

--- setup.d/10-mac-user.sh
+++ setup.d/10-mac-user.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Ignore if we're not on OSX
 [[ "$OSTYPE" =~ ^darwin ]] || exit 0
 
@@ -10,9 +10,9 @@ USER_HOME="$HOME"
 
 # //TODO Update, Possibly by including a user photo in the repo
 PROFILE_PICTURE="https://pbs.twimg.com/profile_images/593907811811078144/Ayn86sDz.jpg"
-sudo dscl . delete $USER_HOME jpegphoto
-sudo dscl . delete $USER_HOME Picture
+#sudo dscl . delete $USER_HOME jpegphoto
+#sudo dscl . delete $USER_HOME Picture
 
 # Pulls down the user icon and names it "login.png"
-curl -o "$USER_HOME/Pictures/login.png" "$PROFILE_PICTURE"
-sudo dscl . create "$USER_HOME" Picture "$USER_HOME/Pictures/login.png"
+#curl -o "$USER_HOME/Pictures/login.png" "$PROFILE_PICTURE"
+#sudo dscl . create "$USER_HOME" Picture "$USER_HOME/Pictures/login.png"
