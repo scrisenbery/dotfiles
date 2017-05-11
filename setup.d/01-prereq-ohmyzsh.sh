9d64b9f53642f9a2e89b8a909fcf1c7597bcb6ee
Scott Crisenbery <scott@scrisenbery.net>
2017-05-10 17:29:57 -0700

    Restore .zshrc after OMZ install overrides it #2

--- setup.d/01-prereq-ohmyzsh.sh
+++ setup.d/01-prereq-ohmyzsh.sh
@@ -2,3 +2,8 @@
 # Install Oh-My-ZSH
 echo "Install oh-my-zsh/n/n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Restore .zshrc after OMZ installation
+if [ -e ~/.zshrc.pre-oh-my-zsh ]; then
+  mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+fi
