8550817e5bdd511dc532a9284b7d986388e3f9f6
Scott Crisenbery <scott@scrisenbery.net>
2017-05-25 16:32:29 -0700

    Fix Powerline fonts install

--- setup.d/01-prereq-powerlinefonts.sh
+++ setup.d/01-prereq-powerlinefonts.sh
@@ -6,7 +6,7 @@ echo "Installing Powerline Fonts"
 echo "--------"
 
 git clone https://github.com/powerline/fonts.git /tmp/powerline-fonts
-source /tmp/powerline-fonts/install.sh
+sh /tmp/powerline-fonts/install.sh
 
 echo "Cleaning up temp dir /tmp/powerline-fonts"
 rm -rf /tmp/powerline-fonts
