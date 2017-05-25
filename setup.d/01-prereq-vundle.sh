ed2364fa7023678ad207198beb77a9077467b4dd
Scott Crisenbery <scott@scrisenbery.net>
2017-05-25 16:31:21 -0700

    Install vim plugins after Vundle install in script

--- setup.d/01-prereq-vundle.sh
+++ setup.d/01-prereq-vundle.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 # Vim Package Manager Install
 
+# //TODO Add checks for issues? (i.e. vim missing, dotfiles missing, etc)
+
 echo "--------"
 echo "Installing Vundle"
 echo "--------"
@@ -9,3 +11,6 @@ if [ ! -d $HOME/.vim/bundle/Vundle.vim ]
 then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
+
+# Install vim plugins via command line
+vim +PluginInstall +qall
