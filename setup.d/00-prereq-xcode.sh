82ce3420c0b6e21c29fe24670a278e3320f4e936
Scott Crisenbery <scott@scrisenbery.net>
2017-05-30 22:00:35 -0700

    Update for #20, commented out

--- setup.d/00-prereq-xcode.sh
+++ setup.d/00-prereq-xcode.sh
@@ -2,3 +2,7 @@
 
 #Check if installed, install if not (???)
 #Accept CLI user agreement stuff (see brew requirements)
+
+# //TODO Need to make sure the script does not continue until this is done if necessary
+#xcode-select --install
+
