8b485f92462f4a966d2d645968102c99a744717f
Scott Crisenbery <scott@scrisenbery.net>
2017-08-01 11:41:32 -0700

    Added XCode setup stuff based on remote-setup.sh

--- setup.d/00-prereq-xcode.sh
+++ setup.d/00-prereq-xcode.sh
@@ -1,8 +1,19 @@
-# //TODO
+# //TODO Look for better ways to do this
+# //TODO Confirm XCode behavior and that this works as expected, especially around what happens if XCode is already installed
 
-#Check if installed, install if not (???)
-#Accept CLI user agreement stuff (see brew requirements)
+# Pulled from remote-setup.sh from @Juxtaposedwords
 
-# //TODO Need to make sure the script does not continue until this is done if necessary
-#xcode-select --install
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+
+  xcode-select --install
+
+  # Ask for administrator password to provide a way for the script to not continue until XCode actions are complete
+  echo "Do not enter your password until XCode installation is complete and the User Agreement has been accepted."
+  echo "This is required for Homebrew to work."
+  echo "XCode should have opened another window to continue its setup."
+  echo "Once XCode setup is complete, enter your administrator password:"
+
+  sudo -v
+
+fi
 
