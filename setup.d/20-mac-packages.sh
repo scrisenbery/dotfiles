1f5ca073e3b8cd9965e3ba15339c3d9b1695cf2c
Scott Crisenbery <scott@scrisenbery.net>
2018-04-27 13:33:36 -0700

    Commented about a lot of the GNU/Linux tool installs, need to review these

--- setup.d/20-mac-packages.sh
+++ setup.d/20-mac-packages.sh
@@ -15,21 +15,21 @@ brew update
 
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils # //TODO
+#brew install coreutils # //TODO
 
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils --with-default-names # //TODO
+#brew install findutils --with-default-names # //TODO
 
 # Install more recent versions of some OS X tools and #JustSysAdminThings //TODO
-brew install gnu-indent --with-default-names
-brew install gnu-sed --with-default-names
-brew install gnutls
-brew install gnu-tar --with-default-names
-brew install gnu-which --with-default-names
-brew install grep --with-default-names
-brew install gawk
-brew install tree
-brew install htop
+#brew install gnu-indent --with-default-names
+#brew install gnu-sed --with-default-names
+#brew install gnutls
+#brew install gnu-tar --with-default-names
+#brew install gnu-which --with-default-names
+#brew install grep --with-default-names
+#brew install gawk
+#brew install tree
+#brew install htop
 
 # Install some recreational stuff
 brew install terminal-notifier
@@ -38,13 +38,13 @@ brew install lastpass-cli
 # This will install all the stuff we need for reasons
 #brew cask install google-chrome
 brew cask install shiftit
-brew cask install alfred
+#brew cask install alfred
 #brew cask install google-drive
-brew cask install skype
+#brew cask install skype
 brew cask install slack
 #brew cask install wireshark
 #brew cask install vlc
-brew cask install opera
+#brew cask install opera
 brew cask install quassel
 
 # Work stuff
