e980bd39356f834acdac12c3ccccff5d7d813c1d
Scott Crisenbery <scrisenbery@users.noreply.github.com>
2022-12-20 14:14:04 -0500

    Add note on machine restart [#29]

--- setup.d/50-post-notes.sh
+++ setup.d/50-post-notes.sh
@@ -5,15 +5,19 @@ echo "=========="
 echo "=========="
 echo "=========="
 echo "\n Post-Install Notes"
-echo "-----"
 
+echo "-----"
+# General
+echo "General: You may need to restart the machine to reflect OS setting updates"
+
+echo "-----"
+# Hammerspoon
 echo "Hammerspoon: Launch, Grant Accessibility permissions in MacOS settings"
 echo "Hammerspoon: Open ~/Downloads/ShiftIt.spoon"
 echo "Hammerspoon: add the config from here, then reload: https://github.com/peterklijn/hammerspoon-shiftit#step-3"
 
-
 echo "-----"
-
+# iTerm
 echo "iTerm2: Change font to Droid Sans Mono for Powerline, size 13 (Prefs > Profiles > Text)"
 echo "iTerm2: Set Prefs > General > Selection > Apps may access clipboard"
 echo "iTerm2: Set Prefs > Advanced > Work around Big Sur bug where a white line flashes at the top of the screen in full screen mode"
